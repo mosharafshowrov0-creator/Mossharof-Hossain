27fed6a2e77946d56e67e1d082b8b4a31f7e8d12
mosharafshowrov0-creator <mosharafshowrov0@gmail.com>
2026-05-05 04:00:43 +0800

    Add files via upload

---
+++ Worksheet1 (2).py
@@ -0,0 +1,22 @@
+a = 15
+b = 12
+
+print(type(a))
+print(type(b))
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+c = int(a / b)
+print(c, type(c))
+
+c = float(c)
+print(c, type(c))
+
+message = "The result of a divided by b is "
+print(message + str(c))
+
+print(a > b)
+print(a == b)
